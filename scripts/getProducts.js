214418d5afe40a1f2a6c576886e346390622546b
yaarslv <savenkovyaroslav53@gmail.com>
2023-10-23 23:11:40 +0300

    Feat: added communication with backend

--- scripts/getProducts.js
+++ scripts/getProducts.js
@@ -1,10 +1,15 @@
+const allBrands = new Set();
+
 async function fetchAndDisplayProducts() {
-    const loader = document.querySelector('.loader');
+    const loaders = document.querySelectorAll('.loader');
     const errorMessageBox = document.querySelector('.error-message');
     const productList = document.querySelector('.product-list');
 
     try {
-        loader.style.display = 'block';
+        loaders.forEach(loader => {
+            loader.style.display = 'block';
+        })
+
         errorMessageBox.style.display = 'none';
 
         await new Promise(resolve => setTimeout(resolve, 2000));
@@ -41,16 +46,21 @@ async function fetchAndDisplayProducts() {
 
                 const priceDiv = document.createElement('div');
                 priceDiv.className = 'product-price';
-                priceDiv.textContent = `Цена: ${product.price} руб`;
+                priceDiv.textContent = `Цена: ${product.price} руб.`;
 
                 const categoryDiv = document.createElement('div');
                 categoryDiv.className = 'product-category';
                 categoryDiv.textContent = product.category;
                 categoryDiv.style.display = 'none';
 
+                const brandDiv = document.createElement('div');
+                brandDiv.className = 'product-brand';
+                brandDiv.textContent = product.brand;
+                brandDiv.style.display = 'none';
+
                 const availabilityDiv = document.createElement('div');
                 availabilityDiv.className = 'product-availability';
-                availabilityDiv.textContent = `${product.availability ? 'Да' : 'Нет в наличии'}`;
+                availabilityDiv.textContent = `${product.availability ? 'В наличии' : 'Нет в наличии'}`;
                 availabilityDiv.style.display = `${product.availability ? 'none' : 'initial'}`;
 
                 const countDiv = document.createElement('div');
@@ -62,6 +72,7 @@ async function fetchAndDisplayProducts() {
                 productDiv.appendChild(nameDiv);
                 productDiv.appendChild(priceDiv);
                 productDiv.appendChild(categoryDiv);
+                productDiv.appendChild(brandDiv);
                 productDiv.appendChild(availabilityDiv);
                 productDiv.appendChild(countDiv);
                 productList.appendChild(productDiv);
@@ -74,13 +85,37 @@ async function fetchAndDisplayProducts() {
                         window.open(`product.html?id=${productId}`);
                     });
                 });
+
+                const brandName = brandDiv.textContent;
+                allBrands.add(brandName);
+            });
+
+            const brandFilter = document.getElementById('brand-filter');
+
+            allBrands.forEach(brandName => {
+                const input = document.createElement('input');
+                input.type = 'checkbox';
+                input.id = `brand-${brandName}`;
+                input.value = brandName;
+
+                const label = document.createElement('label');
+                label.htmlFor = `brand-${brandName}`;
+                label.textContent = brandName;
+
+                brandFilter.appendChild(input);
+                brandFilter.appendChild(label);
+
+                const lineBreak = document.createElement('br');
+                brandFilter.appendChild(lineBreak);
             });
         }
     } catch (error) {
         console.error(error);
         errorMessageBox.style.display = 'block';
     } finally {
-        loader.style.display = 'none';
+        loaders.forEach(loader => {
+            loader.style.display = 'none';
+        })
     }
 }
 
@@ -103,8 +138,18 @@ document.addEventListener("DOMContentLoaded", function () {
             selectedCategories.push('Для черепах');
         }
 
+        const brandFilter = document.getElementById('brand-filter');
+        const selectedBrands = Array.from(brandFilter.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
         const minPrice = parseInt(document.getElementById('minPrice').value, 10);
         const maxPrice = parseInt(document.getElementById('maxPrice').value, 10);
+
+        const selectedAvailabilities = [];
+        if (document.getElementById('in-stock').checked) {
+            selectedAvailabilities.push('В наличии');
+        }
+        if (document.getElementById('out-of-stock').checked) {
+            selectedAvailabilities.push('Нет в наличии');
+        }
 
         const products = document.querySelectorAll('.product');
         products.forEach(product => {
@@ -112,13 +157,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         products.forEach(product => {
+
             const categoryName = product.querySelector('.product-category').textContent;
+            const brandName = product.querySelector('.product-brand').textContent;
             const price = parseInt(product.querySelector('.product-price').textContent.split(' ')[1], 10);
+            const availability = product.querySelector('.product-availability').textContent;
 
             if (
                 (selectedCategories.length === 0 || selectedCategories.includes(categoryName)) &&
+                (selectedAvailabilities.length === 0 || selectedAvailabilities.includes(availability)) &&
                 (!minPrice || price >= minPrice) &&
-                (!maxPrice || price <= maxPrice)
+                (!maxPrice || price <= maxPrice) &&
+                (selectedBrands.length === 0 || selectedBrands.includes(brandName))
             ) {
                 product.style.display = 'block';
             }
@@ -129,14 +179,21 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     const resetFiltersButton = document.querySelector('.resetFiltersButton');
 
-    resetFiltersButton.addEventListener('click', function() {
+    resetFiltersButton.addEventListener('click', function () {
         document.getElementById('filter-cat').checked = false;
         document.getElementById('filter-dog').checked = false;
         document.getElementById('filter-parrot').checked = false;
         document.getElementById('filter-turtle').checked = false;
 
+        document.getElementById('in-stock').checked = false;
+        document.getElementById('out-of-stock').checked = false;
+
+
         document.getElementById('minPrice').value = '';
         document.getElementById('maxPrice').value = '';
+        document.getElementById('brand-filter').querySelectorAll('input[type="checkbox"]:checked').forEach(function(checkbox) {
+            checkbox.checked = false;
+        });
 
         const products = document.querySelectorAll('.product');
         products.forEach(product => {
--- scripts/loading.js
+++ scripts/loading.js
@@ -1,18 +1,26 @@
-const loadingText = document.querySelector('.loading-text');
+const loadingTexts = document.querySelectorAll('.loading-text');
 let animationStep = 0;
 
 function updateLoadingText() {
     if (animationStep === 0) {
-        loadingText.textContent = 'Загрузка';
+        loadingTexts.forEach(loadingText => {
+            loadingText.textContent = 'Загрузка';
+        })
         animationStep = 1;
     } else if (animationStep === 1) {
-        loadingText.textContent = 'Загрузка.';
+        loadingTexts.forEach(loadingText => {
+            loadingText.textContent = 'Загрузка.';
+        })
         animationStep = 2;
     } else if (animationStep === 2) {
-        loadingText.textContent = 'Загрузка..';
+        loadingTexts.forEach(loadingText => {
+            loadingText.textContent = 'Загрузка..';
+        })
         animationStep = 3;
     } else if (animationStep === 3) {
-        loadingText.textContent = 'Загрузка...';
+        loadingTexts.forEach(loadingText => {
+            loadingText.textContent = 'Загрузка...';
+        })
         animationStep = 0;
     }
 }
