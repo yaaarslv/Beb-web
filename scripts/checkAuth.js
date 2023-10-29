1825df4148cf594228cfc0011f7b2230035d46ae
yaarslv <savenkovyaroslav53@gmail.com>
2023-10-29 20:03:29 +0300

    Lab5: Done

--- scripts/checkAuth.js
+++ scripts/checkAuth.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const authForm = document.querySelector('.auth-form');
         authForm.action = 'profile.html'
     } else {
-        if (window.location.href.includes('reviews.html')){
+        if (window.location.href.includes('reviews')){
             const button = document.querySelector('.add-review-button');
             button.style.backgroundColor = "#ff001e"
-        } else if (window.location.href.includes('manage-users.html') ||
-            window.location.href.includes('profile.html') || window.location.href.includes('subscription.html')  ||
-            window.location.href.includes('add-news.html')  || window.location.href.includes('add-product.html') ||
-            window.location.href.includes('manage-products.html') || window.location.href.includes('manage-news.html')) {
+        } else if (window.location.href.includes('manage-users') ||
+            window.location.href.includes('profile') || window.location.href.includes('subscription')  ||
+            window.location.href.includes('add-news')  || window.location.href.includes('add-product') ||
+            window.location.href.includes('manage-products') || window.location.href.includes('manage-news')) {
             window.location.href = 'auth.html';
         }
     }
